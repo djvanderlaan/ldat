af53c10a5de3525cd6ae7f3ec366c137f5d52be9
djvanderlaan <djvanderlaan@unrealizedtime.nl>
2017-10-23 16:12:31 +0200

    Fixed out of bounds in match for zero length vectors

--- src/match.cpp
+++ src/match.cpp
@@ -53,24 +53,28 @@ class match_visitor : public ldat::lvec_visitor {
       compare_lt<T> less_than;
       compare_eq<T> equal;
       
-      ldat::vec::vecsize j = 0;
-      ldat::vec::vecsize index_tab = order_tab_->get_of_type(j, double())-1.0;
-      T el_tab = tab_->get_of_type(index_tab, cppr::base_type(T()));
-      for (ldat::vec::vecsize i = 0; i != size; ++i) {
-        ldat::vec::vecsize index_vec = order_->get_of_type(i, double())-1.0;
-        T el = vec.get(index_vec);
-        while (less_than(el_tab, el) && j < (tab_->size()-1)) {
-          ++j;
-          index_tab = order_tab_->get_of_type(j, double())-1.0;
-          el_tab = tab_->get_of_type(index_tab, cppr::base_type(T()));
-
-        }
-        if (equal(el, el_tab, na_incomparable_)) {
-          result->set(index_vec, index_tab + 1.0);
-        } else {
-          result->set(index_vec, cppr::na<double>());
+      if (size > 0 && tab_->size() > 0) {
+      
+        ldat::vec::vecsize j = 0;
+        ldat::vec::vecsize index_tab = order_tab_->get_of_type(j, double())-1.0;
+        T el_tab = tab_->get_of_type(index_tab, cppr::base_type(T()));
+        for (ldat::vec::vecsize i = 0; i != size; ++i) {
+          ldat::vec::vecsize index_vec = order_->get_of_type(i, double())-1.0;
+          T el = vec.get(index_vec);
+          while (less_than(el_tab, el) && j < (tab_->size()-1)) {
+            ++j;
+            index_tab = order_tab_->get_of_type(j, double())-1.0;
+            el_tab = tab_->get_of_type(index_tab, cppr::base_type(T()));
+  
+          }
+          if (equal(el, el_tab, na_incomparable_)) {
+            result->set(index_vec, index_tab + 1.0);
+          } else {
+            result->set(index_vec, cppr::na<double>());
+          }
         }
       }
+      
       // copy temporary result to result
       if (result_) delete result_;
       result_ = result.release();
